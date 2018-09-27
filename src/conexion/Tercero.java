2d5313d5881f2b5cf2bd57c5bdd4216584f917da
USUARIO <USUARIO@pc4-sala210>
2018-09-26 21:49:10 -0500

    Se crea el metodo consultal

--- src/conexion/Tercero.java
+++ src/conexion/Tercero.java
@@ -4,7 +4,7 @@ import java.sql.*;
 import Terceros.*;
 
 public class Tercero extends TerceroPOA {
-    Conexion objConec= new Conexion();
+    Conexion1 objConec= new Conexion1();
 
     @Override
     public boolean insertarTercero(String nombres, String apellidos, String telefonos) {
@@ -23,7 +23,25 @@ public class Tercero extends TerceroPOA {
 
     @Override
     public String consultarTercero(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         
+        String resultado="";
+        try {
+            String sqlConsultar = "Select * from terceros where in="+ id;
+            objConec.conectar();
+            Statement st=objConec.conex.createStatement();
+            ResultSet rs=st.executeQuery(sqlConsultar);
+            while(rs.next()){
+                resultado += rs.getString(2)+"-"
+                        +rs.getString(3)+ "-"
+                        +rs.getNString(4);
+            }
+            
+        }catch(Exception e){
+            System.out.println("Error"+e.getMessage());
+        } 
+            
+    
+    return resultado;
     }
 
     @Override
